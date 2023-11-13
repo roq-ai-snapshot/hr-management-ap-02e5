ba1baa575e3a54340b55c9d7721b3dc151405185
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-11-13 04:20:12 +0000

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,27 @@
+interface AppConfigInterface {
+  ownerRoles: string[];
+  customerRoles: string[];
+  tenantRoles: string[];
+  tenantName: string;
+  applicationName: string;
+  addOns: string[];
+  ownerAbilities: string[];
+  customerAbilities: string[];
+  getQuoteUrl: string;
+}
+export const appConfig: AppConfigInterface = {
+  ownerRoles: ['Owner'],
+  customerRoles: [],
+  tenantRoles: ['Owner'],
+  tenantName: 'Organization',
+  applicationName: 'HR Management Application',
+  addOns: ['file upload', 'chat', 'notifications', 'file'],
+  customerAbilities: [],
+  ownerAbilities: [
+    'Manage user information',
+    'Manage organization information',
+    'Edit personal profile',
+    "Manage organization's users",
+  ],
+  getQuoteUrl: 'https://app.roq.ai/proposal/abbb1375-9f52-49de-9345-ec0df18facd7',
+};
